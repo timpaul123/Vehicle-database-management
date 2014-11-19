66e640768906e2677f4c2fe155efdc3a06ee77f5
timpaul123 <timpaul7@hotmail.com>
2014-11-19 18:41:31 +0000

    Update Date.java

--- classes/Date.java
+++ classes/Date.java
@@ -1,1 +1,52 @@
-x
+/*
+ * A simple Date class
+ */
+
+package databasemanagement;
+
+/**
+ *
+ * @author Tim Carrington
+ */
+public class Date {
+    
+    private int day;
+    private int month;
+    private int year;
+    
+    private static final int daysPerMonth[] 
+                                    = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+    
+    public Date(int day, int month, int year){
+        if(month < 1 || month > 12){
+            throw new IllegalArgumentException("Invalid month value. Month must be between 1-12");
+            }
+        if(day < 1 || ((day > daysPerMonth[month]) && !(month == 2 && day == 29))){
+                throw new IllegalArgumentException ("Invalid day for the month");
+            }
+        if(month == 2 && day == 29 && !(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))){
+            throw new IllegalArgumentException ("Invalid day for month and year");
+        }
+    
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+    
+    public int getDay(){
+        return day;
+    }
+    
+    public int getMonth(){
+        return month;
+    }
+    
+    public int getYear(){
+        return year;
+    }
+    
+    @Override
+    public String toString(){
+        return String.format("%d/%d/%d", day, month, year);
+    }
+}
